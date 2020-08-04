42f51ec7427b55e8ceae7337dca2b6775f8a3ed6
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-08-04 17:55:06 +0500

    4/8/2020 6:00 pm

--- Day 25 - Running Time and Complexity/Solution.java
+++ Day 25 - Running Time and Complexity/Solution.java
@@ -1,10 +1,10 @@
- 
-
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+  Scanner scan = new Scanner(System.in);
         int T = scan.nextInt();
         while (T-- > 0) {
             int n = scan.nextInt();
