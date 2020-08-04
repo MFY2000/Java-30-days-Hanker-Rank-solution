6c1b537abb1f555e5720461e6030abad4a0a5958
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-08-04 17:54:48 +0500

    4/8/2020 6:00 pm

--- Day 23 - BST Level-Order Traversal/Solution.java
+++ Day 23 - BST Level-Order Traversal/Solution.java
@@ -1,20 +1,17 @@
- 
-
-import java.io.*;
 import java.util.*;
-
-class Node {
-    Node left, right;
+import java.io.*;
+class Node{
+    Node left,right;
     int data;
-    Node(int data) {
-        this.data = data;
-        left = right = null;
+    Node(int data){
+        this.data=data;
+        left=right=null;
     }
 }
+class Solution{
 
-class Solution {
-    static void levelOrder(Node root) {
-        if (root == null) {
+static void levelOrder(Node root){
+    if (root == null) {
             return;
         }
         ArrayDeque<Node> deque = new ArrayDeque(); // use deque as a queue
@@ -28,34 +25,34 @@ class Solution {
             if (n.right != null) {
                 deque.addLast(n.right);
             }
-        }
+        }  
     }
 
-
-    public static Node insert(Node root,int data){
-        if (root == null) {
+public static Node insert(Node root,int data){
+        if(root==null){
             return new Node(data);
-        } else {
+        }
+        else{
             Node cur;
-            if (data <= root.data) {
-                cur = insert(root.left, data);
-                root.left = cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
             }
-            else {
-                cur = insert(root.right, data);
-                root.right = cur;
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
             }
             return root;
         }
     }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        Node root = null;
-        while (T-- > 0) {
-            int data = sc.nextInt();
-            root = insert(root, data);
-        }
-        levelOrder(root);
-    }   
+    public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            int T=sc.nextInt();
+            Node root=null;
+            while(T-->0){
+                int data=sc.nextInt();
+                root=insert(root,data);
+            }
+            levelOrder(root);
+        }	
 }
