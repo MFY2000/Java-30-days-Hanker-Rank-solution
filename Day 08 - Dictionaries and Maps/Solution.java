395955546aaf31faff9a7eef260711ff2ad7f49e
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-08-04 17:03:33 +0500

    4/8/2020 3:00 pm

--- Day 08 - Dictionaries and Maps/Solution.java
+++ Day 08 - Dictionaries and Maps/Solution.java
@@ -1,30 +1,25 @@
- 
 
-import java.util.Scanner;
-import java.util.HashMap;
+import java.util.*;
+import java.io.*;
 
-class Solution {
-    public static void main(String[] args) {
-        /* Save input as entries in a HashMap */
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
         HashMap<String, Integer> map = new HashMap();
         for (int i = 0; i < n; i++) {
-            String name = scan.next();
-            int phone   = scan.nextInt();
+            String name = in.next();
+            int phone   = in.nextInt();
             map.put(name, phone);
         }
-        
-        /* Read each query and check if its in our HashMap */
-        while (scan.hasNext()) {
-            String str = scan.next();
+        while(in.hasNext()){
+            String str = in.next();
             if (map.containsKey(str)) {
                 System.out.println(str + "=" + map.get(str));
             } else {
                 System.out.println("Not found");
             }
         }
-        
-        scan.close();
+        in.close();
     }
 }
