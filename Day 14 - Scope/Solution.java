0e3c86df69e180feca52386135e8e470e4eff9d0
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-08-04 17:53:26 +0500

    4/8/2020 6:00 pm

--- Day 14 - Scope/Solution.java
+++ Day 14 - Scope/Solution.java
@@ -1,16 +1,14 @@
- 
-
 import java.io.*;
 import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+
 class Difference {
-    private int[] elements;
-    public int maximumDifference;
-    
-    Difference (int [] elements) {
+  	private int[] elements;
+  	public int maximumDifference;
+ Difference (int [] elements) {
         this.elements = elements;
     }
 
@@ -24,6 +22,7 @@ class Difference {
 } // End of Difference class
 
 public class Solution {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
