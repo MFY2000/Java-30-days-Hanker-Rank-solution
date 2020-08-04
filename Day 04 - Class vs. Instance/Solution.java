c9c2ce25f346ca6301d899c6aab8289284595f91
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-08-04 17:02:55 +0500

    4/8/2020 3:00 pm

--- Day 04 - Class vs. Instance/Solution.java
+++ Day 04 - Class vs. Instance/Solution.java
@@ -1,5 +1,3 @@
- 
-
 import java.io.*;
 import java.util.*;
 
@@ -28,19 +26,20 @@ public class Person {
         age++;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for (int i = 0; i < T; i++) {
-            int age = sc.nextInt();
-            Person p = new Person(age);
-            p.amIOld();
-            for (int j = 0; j < 3; j++) {
-                p.yearPasses();
-            }
-            p.amIOld();
-            System.out.println();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int i = 0; i < T; i++) {
+			int age = sc.nextInt();
+			Person p = new Person(age);
+			p.amIOld();
+			for (int j = 0; j < 3; j++) {
+				p.yearPasses();
+			}
+			p.amIOld();
+			System.out.println();
         }
-        sc.close();
+		sc.close();
     }
 }
