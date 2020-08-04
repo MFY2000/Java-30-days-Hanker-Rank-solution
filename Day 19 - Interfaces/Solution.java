011aa00a37a472aaa0578e4868eec61142c2a481
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-08-04 17:54:10 +0500

    4/8/2020 6:00 pm

--- Day 19 - Interfaces/Solution.java
+++ Day 19 - Interfaces/Solution.java
@@ -1,16 +1,11 @@
- 
-
 import java.io.*;
 import java.util.*;
 
-interface AdvancedArithmetic {
+interface AdvancedArithmetic{
    int divisorSum(int n);
 }
-
 class Calculator implements AdvancedArithmetic {
-    //          Runtime: O(n^(1/2))
-    // Space Complexity: O(1)
-    public int divisorSum(int n) {
+     public int divisorSum(int n) {
         int sum = 0;
         int sqrt = (int) Math.sqrt(n);
 
@@ -28,16 +23,17 @@ class Calculator implements AdvancedArithmetic {
             sum -= sqrt;
         }
         return sum;
-    }
+    } 
 }
 
 class Solution {
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         scan.close();
         
-        AdvancedArithmetic myCalculator = new Calculator(); 
+      	AdvancedArithmetic myCalculator = new Calculator(); 
         int sum = myCalculator.divisorSum(n);
         System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
         System.out.println(sum);
