d5133c120cc8bf6c3c97effd81e0ce14e50f31da
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-08-04 17:03:14 +0500

    4/8/2020 3:00 pm

--- Day 06 - Let's Review/Solution.java
+++ Day 06 - Let's Review/Solution.java
@@ -1,10 +1,10 @@
- 
-
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);
         int T = scan.nextInt();
         for (int i = 0; i < T; i++) {
             String str = scan.next();
