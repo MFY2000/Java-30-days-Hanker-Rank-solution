8964af18f8989fff6fe5ae08b61b2975beee4b09
MFY2000 <fa19bsse0014@maju.edu.pk>
2020-08-04 17:54:59 +0500

    4/8/2020 6:00 pm

--- Day 24 - More Linked Lists/Solution.java
+++ Day 24 - More Linked Lists/Solution.java
@@ -1,20 +1,19 @@
- 
-
 import java.io.*;
 import java.util.*;
-
-class Node {
-    int data;
-    Node next;
-    Node(int d) {
-        data = d;
-        next = null;
+class Node{
+	int data;
+	Node next;
+	Node(int d){
+        data=d;
+        next=null;
     }
+	
 }
+class Solution
+{
 
-class Solution {
     public static Node removeDuplicates(Node head) {
-        if (head == null) {
+       if (head == null) {
             return null;
         }
         Node n = head;
@@ -28,38 +27,43 @@ class Solution {
         return head;
     }
 
-    public static Node insert(Node head,int data) {
-        Node p = new Node(data);
-        if (head == null)
-            head = p;
-        else if (head.next == null)
-            head.next = p;
-        else {
-            Node start = head;
-            while (start.next != null)
-                start = start.next;
-            start.next = p;
+	 public static  Node insert(Node head,int data)
+     {
+        Node p=new Node(data);			
+        if(head==null)
+            head=p;
+        else if(head.next==null)
+            head.next=p;
+        else
+        {
+            Node start=head;
+            while(start.next!=null)
+                start=start.next;
+            start.next=p;
+
         }
         return head;
     }
-
-    public static void display(Node head) {
-        Node start = head;
-        while (start != null) {
-            System.out.print(start.data + " ");
-            start = start.next;
+    public static void display(Node head)
+        {
+              Node start=head;
+              while(start!=null)
+              {
+                  System.out.print(start.data+" ");
+                  start=start.next;
+              }
         }
+        public static void main(String args[])
+        {
+              Scanner sc=new Scanner(System.in);
+              Node head=null;
+              int T=sc.nextInt();
+              while(T-->0){
+                  int ele=sc.nextInt();
+                  head=insert(head,ele);
+              }
+              head=removeDuplicates(head);
+              display(head);
+
+       }
     }
-
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        Node head = null;
-        int T = sc.nextInt();
-        while (T-- > 0) {
-            int ele = sc.nextInt();
-            head = insert(head, ele);
-        }
-        head = removeDuplicates(head);
-        display(head);
-   }
-}
